cdff69f8bb80a65582887aa6f191625b3b7f0071
Adam Dinwoodie <adam@dinwoodie.org>
2025-06-02 16:19:16 +0100

    git master: init at 2.50.0.rc0.46.g7014b55638

--- versions.nix
+++ versions.nix
@@ -4,4 +4,9 @@
     hash = "sha256-6QyoR28Af6cWtZ/AMx8Rxr8rTK4Yidf1XdZCu2imAr0=";
     version = "2.50.0.rc0.629.g846fc57c9e";
   };
+  master = {
+    rev = "7014b55638da979331baf8dc31c4e1d697cf2d67";
+    hash = "sha256-/6+1o9wQGPjzMDSAROD3/6cCKPijrTJwcjv0D0uMa1c=";
+    version = "2.50.0.rc0.46.g7014b55638";
+  };
 }
