a63e8788b028114058a1969ef968759f3b1d3322
Adam Dinwoodie <adam@dinwoodie.org>
2025-08-22 10:49:01 +0100

    git maint-2.51: init at 2.51.0

--- versions.nix
+++ versions.nix
@@ -9,6 +9,14 @@ let
       rev = "032396e0dae35802b404c2aba2b08cae8a518907";
       hash = "sha256-7MrkXirWEsZmUrdzFkSAXEk5ZNTYx8lfKPOmC9WJ5oU=";
       version = "2.51.0.308.g032396e0da";
+
+      # TODO Remove these once they're no longer included in any Nixpkgs
+      # channel I care about building against.
+      #
+      # TODO Set things up so that I can have the overrides depend on the
+      # channel I'm building against, as at time of writing I believe none of
+      # these changes are necessary for nixpkgs-unstable, or at least they
+      # won't be once the current staging branch is merged.
       extraOverride = prevAttrs: {
         patches = map (
           p:
@@ -73,11 +81,43 @@ let
       };
       priority = 1;
     };
+    "maint-2.51" = {
+      rev = "c44beea485f0f2feaf460e2ac87fdd5608d63cf0";
+      hash = "sha256-vMjFXlOVs2OgNGn/t32Quwqo7qLYfgPaAka048qQ42g=";
+      version = "2.51.0";
+      extraOverride = prevAttrs: {
+        patches = map (
+          p:
+          if baseNameOf p == "git-send-email-honor-PATH.patch" then
+            ./git-send-email-honor-PATH-fixed.patch
+          else
+            p
+        ) prevAttrs.patches;
+        postInstall =
+          builtins.replaceStrings
+            [
+              "\nrm -r contrib/hooks/multimail"
+              "\nmkdir -p $out/share/git-core/contrib"
+              "\ncp -a contrib/hooks/ $out/share/git-core/contrib/"
+              "\nsubstituteInPlace $out/share/git-core/contrib/hooks/pre-auto-gc-battery \\"
+              "\n  --replace ' grep' ' /nix/store/"
+            ]
+            [
+              ""
+              ""
+              ""
+              ""
+              "\n# REMOVED FOR EXPEDIENCE"
+            ]
+            prevAttrs.postInstall;
+      };
+      priority = 3;
+    };
     "maint-2.50" = {
       rev = "f368df439b31b422169975cc3c95f7db6a46eada";
       hash = "sha256-Up7l/879PSvA8ntpjdnmBUefK3l/B8rQi+qvGiS50iU=";
       version = "2.50.1.11.gf368df439b";
-      priority = 3;
+      priority = 4;
     };
   };
 in
