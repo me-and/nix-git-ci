7d6614adf1e1289abb2a34a806bcd3d4c2ec57f4
Adam Dinwoodie <adam@dinwoodie.org>
2025-11-19 11:04:56 +0000

    Cut down tests to repro problem

--- checks.nix
+++ checks.nix
@@ -15,37 +15,5 @@ let
   };
 
   packageChecks = lib.mapAttrs (_: v: v.overrideAttrs { doInstallCheck = true; }) gitPackages;
-
-  versionData = import ./versions.nix {
-    inherit pkgs lib channelName;
-  };
-
-  fetchgitCheckFn =
-    versionData:
-    let
-      git = gitPackages."git-${versionData.safeName}";
-      gitMinimal = gitPackages."gitMinimal-${versionData.safeName}";
-      git-lfs = pkgs.git-lfs.override { inherit git; };
-      fetchgit = pkgs.fetchgit.override {
-        inherit git-lfs;
-        # fetchgit is defined in all-packages.nix with git as
-        # gitMinimal.
-        git = gitMinimal;
-      };
-
-      # TODO raise this bug: the underlying test doesn't work in pure
-      # evaluation mode.  Try `nix build nixpkgs#tests.fetchgit.withGitConfig`.
-      brokenTests = [ "withGitConfig" ];
-      baseFetchgitChecks = lib.filterAttrs (
-        n: v: !(builtins.elem n brokenTests) && lib.isDerivation v
-      ) pkgs.tests.fetchgit;
-    in
-    lib.mapAttrs' (
-      n: v: lib.nameValuePair "${n}-${versionData.safeName}" (v.override { inherit fetchgit; })
-    ) baseFetchgitChecks;
-
-  fetchgitChecks = lib.mergeAttrsList (
-    builtins.map fetchgitCheckFn (builtins.attrValues versionData)
-  );
 in
-lib.attrsets.unionOfDisjoint packageChecks fetchgitChecks
+packageChecks
--- packagenames.nix
+++ packagenames.nix
@@ -7,13 +7,4 @@
   git = {
     priority = 1;
   };
-  gitFull = {
-    priority = 2;
-  };
-  gitMinimal = {
-    priority = 3;
-  };
-  gitSVN = {
-    priority = 4;
-  };
 }
