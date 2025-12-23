774614a1f4e3d53c37ba4ae12503c8662c20fbcf
Adam Dinwoodie <adam@dinwoodie.org>
2025-12-23 15:04:20 +0000

    Update patches

--- versions.nix
+++ versions.nix
@@ -19,117 +19,56 @@ let
       hash = "sha256-KoEFBYmgZSjLq+XZS+MoTVRXaLZlMaZRSZfsBJFaRxY=";
       version = "2.52.0.392.gee94076814";
 
-      # TODO Remove these once they're no longer included in any Nixpkgs
-      # channel I care about building against.
-      #
-      # TODO Set things up so that I can have the overrides depend on the
-      # channel I'm building against, as at time of writing I believe none of
-      # these changes are necessary for nixpkgs-unstable, or at least they
-      # won't be once the current staging branch is merged.
       extraOverride = prevAttrs: {
         patches =
-          map (
-            p:
-            if baseNameOf p == "git-send-email-honor-PATH.patch" then
-              ./git-send-email-honor-PATH-fixed.patch
-            else
-              p
-          ) prevAttrs.patches
-          ++ [ ./t1517.diff ];
-        # Bit of a hack to remove the bits I care about without rewriting the
-        # entire postInstall stage.
-        postInstall =
-          builtins.replaceStrings
-            [
-              "\nrm -r contrib/hooks/multimail"
-              "\nmkdir -p $out/share/git-core/contrib"
-              "\ncp -a contrib/hooks/ $out/share/git-core/contrib/"
-              "\nsubstituteInPlace $out/share/git-core/contrib/hooks/pre-auto-gc-battery \\"
-              "\n  --replace ' grep' ' /nix/store/"
-            ]
-            [
-              ""
-              ""
-              ""
-              ""
-              "\n# REMOVED FOR EXPEDIENCE"
-            ]
-            prevAttrs.postInstall;
+          (prevAttrs.patches or [])
+          ++ [(pkgs.fetchurl {
+                name = "expect-gui--askyesno-failure-in-t1517.patch";
+                    url = "https://lore.kernel.org/git/20251201031040.1120091-1-brianmlyles@gmail.com/raw";
+                        hash = "sha256-vvhbvg74OIMzfksHiErSnjOZ+W0M/T9J8GOQ4E4wKbU=";
+                          })];
       };
+
       priority = 2;
     };
     master = {
       rev = "c4a0c8845e2426375ad257b6c221a3a7d92ecfda";
       hash = "sha256-KvecMtusHuTDgE6wz6eRz+2AYIt1Tn3ecfaVV1PIxk8=";
       version = "2.52.0.269.gc4a0c8845e";
+
       extraOverride = prevAttrs: {
         patches =
-          map (
-            p:
-            if baseNameOf p == "git-send-email-honor-PATH.patch" then
-              ./git-send-email-honor-PATH-fixed.patch
-            else
-              p
-          ) prevAttrs.patches
-          ++ [ ./t1517.diff ];
-        # Bit of a hack to remove the bits I care about without rewriting the
-        # entire postInstall stage.
-        postInstall =
-          builtins.replaceStrings
-            [
-              "\nrm -r contrib/hooks/multimail"
-              "\nmkdir -p $out/share/git-core/contrib"
-              "\ncp -a contrib/hooks/ $out/share/git-core/contrib/"
-              "\nsubstituteInPlace $out/share/git-core/contrib/hooks/pre-auto-gc-battery \\"
-              "\n  --replace ' grep' ' /nix/store/"
-            ]
-            [
-              ""
-              ""
-              ""
-              ""
-              "\n# REMOVED FOR EXPEDIENCE"
-            ]
-            prevAttrs.postInstall;
+          (prevAttrs.patches or [])
+          ++ [(pkgs.fetchurl {
+                name = "expect-gui--askyesno-failure-in-t1517.patch";
+                    url = "https://lore.kernel.org/git/20251201031040.1120091-1-brianmlyles@gmail.com/raw";
+                        hash = "sha256-vvhbvg74OIMzfksHiErSnjOZ+W0M/T9J8GOQ4E4wKbU=";
+                          })];
       };
+
       priority = 1;
     };
     "maint-2.52" = {
       rev = "9a2fb147f2c61d0cab52c883e7e26f5b7948e3ed";
       hash = "sha256-2TMwVrb1PIxQSOnl2dR9EzMsSdVvfX5Z9HIpbEaxX94=";
       version = "2.52.0";
+
+      extraOverride = prevAttrs: {
+        patches =
+          (prevAttrs.patches or [])
+          ++ [(pkgs.fetchurl {
+                name = "expect-gui--askyesno-failure-in-t1517.patch";
+                    url = "https://lore.kernel.org/git/20251201031040.1120091-1-brianmlyles@gmail.com/raw";
+                        hash = "sha256-vvhbvg74OIMzfksHiErSnjOZ+W0M/T9J8GOQ4E4wKbU=";
+                          })];
+      };
+
       priority = 3;
     };
     "maint-2.51" = {
       rev = "bb5c624209fcaebd60b9572b2cc8c61086e39b57";
       hash = "sha256-2aOM0jlatuIlxngQyOkkZQ/b8mvuJ9jxUgPduCEyDrk=";
       version = "2.51.2";
-      extraOverride = prevAttrs: {
-        patches = map (
-          p:
-          if baseNameOf p == "git-send-email-honor-PATH.patch" then
-            ./git-send-email-honor-PATH-fixed.patch
-          else
-            p
-        ) prevAttrs.patches;
-        postInstall =
-          builtins.replaceStrings
-            [
-              "\nrm -r contrib/hooks/multimail"
-              "\nmkdir -p $out/share/git-core/contrib"
-              "\ncp -a contrib/hooks/ $out/share/git-core/contrib/"
-              "\nsubstituteInPlace $out/share/git-core/contrib/hooks/pre-auto-gc-battery \\"
-              "\n  --replace ' grep' ' /nix/store/"
-            ]
-            [
-              ""
-              ""
-              ""
-              ""
-              "\n# REMOVED FOR EXPEDIENCE"
-            ]
-            prevAttrs.postInstall;
-      };
       priority = 4;
     };
   };
